a28cab63655d3ec6168fdfdd5c1dc76a30ca897f
Sjoerd Dost <deosjr@gmail.com>
2025-04-18 17:50:52 +0200

    Run 6 up to Dewford

---
+++ run6.pl
@@ -0,0 +1,28 @@
+:- begin_tests(run6).
+%test(youngster_calvin, [nondet]) :-
+    % soloed by Chinchou
+
+test(bug_catcher_rick, [nondet]) :-
+    % soloed by Litleo
+
+%test(youngster_allen, [nondet]) :-
+    % lead Pidgey, then Chinchou takes over
+
+%test(lass_tiana, [nondet]) :-
+    % soloed by Litleo
+
+%test(traithlete_mikey, [nondet]) :-
+    % Chinchou solos Krabbey and Clobbopus, then Vivillon switches in on Yanma and one-shots it.
+
+%test(fisherman_darian, [nondet]) :-
+    % Vivillon solos, using Protect vs Bounce and outspeeding the second Magikarp to kill through Focus Sash
+
+%test(lady_cindy, [nondet]) :-
+    % Vivillon + Chinchou win again, with use of anti-para berries.
+
+%test(team_aqua_grunt, [nondet]) :-
+    % Chinchou kills Carvanha, then we switch and chip into Exeggcute with Pidgey, Fletchling and Litleo.
+    % The latter prevents its berry/harvest combo and kills with Ember.
+    % Now we chip onto Croagunk and kill with Vivillon.
+
+:- end_tests(run6).
